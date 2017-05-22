a75c9207f8363e129d9ad0512147e80374787e97
Odin Matanguihan <odin_matanguihan@yahoo.com>
2017-05-22 21:01:20 +0800

    Add files via upload

--- proj.R
+++ proj.R
@@ -1,12 +1,21 @@
+install.packages("caret")
 library(caret)
-dat <- read.csv("pml-training.csv")
-#testing <- read.csv("pml-testing.csv")
+library(randomForest)
 
+testing <- read.csv("pml-testing.csv")
+dat <- read.csv("pml-training.csv")
 
 
 #NArate <- apply(trainingCl, 2, function(x) sum(is.na(x)))/nrow(trainingCl)
-toomuchNA <- apply(training, 2, function(x) sum(is.na(x)))/nrow(training)>.97
-training <- training[,!toomuchNA]
+toomuchNA <- apply(dat, 2, function(x) sum(is.na(x)))/nrow(dat)>.96
+dat <- dat[,!toomuchNA]
+nzvval <- nearZeroVar(dat, saveMetrics= TRUE)$nzv
+dat <- dat[,!nzvval]
+dat <- dat[,7:59]
+dat <- dat[,-6]
+
+
+
 
 #partition
 set.seed(300)
@@ -17,10 +26,11 @@ testing = dat[-inTrain,]
 #create models
 
 forpca <- preProcess(training, method = "pca", thresh = 0.95)
-pcamodel <- train(training, method = "pca", thresh = 0.95)
+pcamodel <- train(classe~., data = training, method = "pca", thresh = 0.95)
 
 
 rfmodel <- train(classe~., data = training,method = 'rf', do.trace = FALSE)
+rfmodel <- randomForest(classe~., data = training)
 gbmmodel <- train(classe~., data = training, method = 'gbm')
 ldamodel <- train(classe~., data = training, method = 'lda')
 svmmodel
@@ -28,3 +38,7 @@ svmmodel
 rfpred <- predict(rfmodel, testing)
 gbmpred <- predict(gbmmodel, testing)
 ldapred <- predict(ldamodel, testing)
+
+confusionMatrix(testing$classe, rfpred)$overall["Accuracy"]
+confusionMatrix(testing$classe, gbmpred)$overall["Accuracy"]
+confusionMatrix(testing$classe, ldapred)$overall["Accuracy"]
