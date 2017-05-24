de092f86889cb89a5e97375251db43461cc753b5
Odin Matanguihan <odin_matanguihan@yahoo.com>
2017-05-24 20:30:58 +0800

    Add files via upload

--- proj.R
+++ proj.R
@@ -1,7 +1,9 @@
 install.packages("caret")
+install.packages("randomForest")
+install.packages("e1071")
 library(caret)
 library(randomForest)
-
+library(e1071)
 testing <- read.csv("pml-testing.csv")
 dat <- read.csv("pml-training.csv")
 
@@ -33,13 +35,25 @@ rfmodel <- train(classe~., data = training,method = 'rf', do.trace = FALSE)
 rfmodel <- randomForest(classe~., data = training)
 gbmmodel <- train(classe~., data = training, method = 'gbm')
 ldamodel <- train(classe~., data = training, method = 'lda')
-svmmodel
+rpartmodel <- train(classe~., data = training, method = 'rpart')
 
 rfpred <- predict(rfmodel, testing)
 gbmpred <- predict(gbmmodel, testing)
 ldapred <- predict(ldamodel, testing)
+rpartpred <- predict(rpartmodel, testing)
+#rfaccuracy 0.997553
 #gbmAccuracy 0.9624796
 #ldaAccuracy 0.7065661
 confusionMatrix(testing$classe, rfpred)$overall["Accuracy"]
 confusionMatrix(testing$classe, gbmpred)$overall["Accuracy"]
 confusionMatrix(testing$classe, ldapred)$overall["Accuracy"]
+confusionMatrix(testing$classe, rpartpred)$overall["Accuracy"]
+
+#stack the predictions
+
+combpred <- data.frame(classe = testing$classe, rfpred, gbmpred, ldapred)
+rfcombmod <- train(classe~., data = combpred, method = 'rf')
+rfcombpred <- predict(rfcombmod, combpred)
+rfcombpred <- predict(rfcombmod, combpred)
+confusionMatrix(testing$classe, rfcombpred)$overall["Accuracy"]
+#Accuracy:  0.997553
